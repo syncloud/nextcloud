04b51f381da053a8718d36ff61ac1dd6764eceb6
Boris Rybalkin <ribalkin@gmail.com>
2017-01-28 14:46:36 +0000

    fix php service

--- bin/service.php-fpm.sh
+++ bin/service.php-fpm.sh
@@ -4,4 +4,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 export LD_LIBRARY_PATH=${DIR}/php/lib:${DIR}/postgresql/lib
 
-exec $DIR/php/sbin/php-fpm -y ${DIR}/config/php/php-fpm.conf -c ${DIR}/config/php/php.ini
+exec $DIR/php/sbin/php-fpm -y ${DIR}/config/php-fpm.conf -c ${DIR}/config/php.ini
