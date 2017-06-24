8a45ea9f7c0c014421563aec21c2cdbd687bde9e
Boris Rybalkin <ribalkin@gmail.com>
2017-06-24 21:57:51 +0100

    debug login page

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -1,15 +1,13 @@
 import os
 import shutil
 from os.path import dirname, join, exists
+
 import pytest
-import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.support.ui import WebDriverWait
 
 DIR = dirname(__file__)
 LOG_DIR = join(DIR, 'log')
@@ -17,6 +15,7 @@ DEVICE_USER = 'user'
 DEVICE_PASSWORD = 'password'
 log_dir = join(LOG_DIR, 'nextcloud_log')
 screenshot_dir = join(DIR, 'screenshot')
+
 
 @pytest.fixture(scope="module")
 def driver():
@@ -36,10 +35,12 @@ def driver():
     profile.add_extension('{0}/JSErrorCollector.xpi'.format(DIR))
     profile.set_preference('app.update.auto', False)
     profile.set_preference('app.update.enabled', False)
-    driver = webdriver.Firefox(profile, capabilities=caps, log_path="{0}/firefox.log".format(LOG_DIR), firefox_binary=binary, executable_path=join(DIR, 'geckodriver/geckodriver'))
-    #driver.set_page_load_timeout(30)
-    #print driver.capabilities['version']
-    return driver
+    driver = webdriver.Firefox(profile,
+                               capabilities=caps, log_path="{0}/firefox.log".format(LOG_DIR),
+                               firefox_binary=binary, executable_path=join(DIR, 'geckodriver/geckodriver'))
+    # driver.set_page_load_timeout(30)
+    # print driver.capabilities['version']
+    return WebDriverWait(driver, 20)
 
 
 def test_web_with_selenium(driver, user_domain):
@@ -51,9 +52,12 @@ def test_web_with_selenium(driver, user_domain):
     password.send_keys(DEVICE_PASSWORD)
     driver.get_screenshot_as_file(join(screenshot_dir, 'login.png'))
     print(driver.page_source.encode('utf-8'))
+    driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.SHIFT + 'k')
+    driver.get_screenshot_as_file(join(screenshot_dir, 'login_debug.png'))
+
     # submit = driver.find_element_by_id("submit")
     # password.submit()
-    #password.send_keys(Keys.RETURN)
+    # password.send_keys(Keys.RETURN)
     # time.sleep(10)
     # driver.get_screenshot_as_file(join(screenshot_dir, 'after_lpgin.png'))
     # wait_driver = WebDriverWait(driver, 30)
