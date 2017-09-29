5b4284d1bb39bb37a9bf8858ac3ab0f506d963d7
Boris Rybalkin <ribalkin@gmail.com>
2017-09-29 08:49:46 +0100

    upgrade requests to support Unix socket for web activation

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ rm -rf ${DIR}/lib
 mkdir ${DIR}/lib
 
 coin --to lib py https://pypi.python.org/packages/2.7/b/beautifulsoup4/beautifulsoup4-4.4.0-py2-none-any.whl
-coin --to lib py https://pypi.python.org/packages/2.7/r/requests/requests-2.7.0-py2.py3-none-any.whl
+coin --to lib py https://pypi.python.org/packages/49/df/50aa1999ab9bde74656c2919d9c0c085fd2b3775fd3eca826012bef76d8c/requests-2.18.4-py2.py3-none-any.whl#md5=eb9be71cc41fd73a51a7c9cd1adde5de
 coin --to lib py https://pypi.python.org/packages/source/m/massedit/massedit-0.67.1.zip
 coin --to lib py https://pypi.python.org/packages/source/s/syncloud-lib/syncloud-lib-2.tar.gz
 
