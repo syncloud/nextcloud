c9b30bbb3aa0da8d70e79775a8979fe16d8843db
Boris Rybalkin <ribalkin@gmail.com>
2018-01-04 23:23:03 +0000

    lazy cron init as app user is not available in the beginning of the install

--- src/nextcloud/cron.py
+++ src/nextcloud/cron.py
@@ -9,21 +9,21 @@ class OwncloudCron:
     def __init__(self, app_dir, data_dir, app_name, cron_user):
         self.cron_cmd = 'DATA_DIR={0} {1}'.format(data_dir, join(app_dir, 'bin/{0}-cron'.format(app_name)))
         self.cron_user = cron_user
-        self.cron = CronTab(user=self.cron_user)
         self.log = logger.get_logger('cron')
 
     def remove(self):
         print("remove crontab task")
-
-        for job in self.cron.find_command(self.cron_user):
-            self.cron.remove(job)
-        self.cron.write()
+        cron = CronTab(user=self.cron_user)
+        for job in cron.find_command(self.cron_user):
+            cron.remove(job)
+        cron.write()
 
     def create(self):
+        cron = CronTab(user=self.cron_user)
         print("create crontab task")
-        ci_job = self.cron.new(command=self.cron_cmd)
+        ci_job = cron.new(command=self.cron_cmd)
         ci_job.setall('*/15 * * * *')
-        self.cron.write()
+        cron.write()
 
     def run(self):
         self.log.info("running: {0}".format(self.cron_cmd))
