1edd880aeafa4c43f4557aa73a30e83fb209f8d7
Boris Rybalkin <ribalkin@gmail.com>
2018-02-08 18:35:51 +0000

    inline cron

--- hooks/installer.py
+++ hooks/installer.py
@@ -1,7 +1,6 @@
 from os.path import dirname, join, abspath, isdir
 from os import listdir
 import sys
-#from cron import OwncloudCron
 
 app_path = abspath(join(dirname(__file__), '..'))
 
@@ -25,6 +24,12 @@ from syncloudlib.application import paths, urls, storage, users
 from postgres import Database
 from octools import OCConsole, OCConfig
 from webface import Setup
+
+from crontab import CronTab
+from subprocess import check_output
+from syncloud_app import logger
+from os.path import join
+
 
 APP_NAME = 'nextcloud'
 
@@ -241,3 +246,29 @@ class NextcloudInstaller:
         domains = ['localhost', local_ip, app_domain]
         oc_config = OCConfig(join(self.app.get_install_dir(), OC_CONFIG_PATH))
         oc_config.set_value('trusted_domains', " ".join(domains))
+
+
+class OwncloudCron:
+
+    def __init__(self, app_dir, data_dir, app_name, cron_user):
+        self.cron_cmd = 'DATA_DIR={0} {1}'.format(data_dir, join(app_dir, 'bin/{0}-cron'.format(app_name)))
+        self.cron_user = cron_user
+        self.log = logger.get_logger('cron')
+
+    def remove(self):
+        print("remove crontab task")
+        cron = CronTab(user=self.cron_user)
+        for job in cron.find_command(self.cron_user):
+            cron.remove(job)
+        cron.write()
+
+    def create(self):
+        cron = CronTab(user=self.cron_user)
+        print("create crontab task")
+        ci_job = cron.new(command=self.cron_cmd)
+        ci_job.setall('*/15 * * * *')
+        cron.write()
+
+    def run(self):
+        self.log.info("running: {0}".format(self.cron_cmd))
+        self.log.info(check_output('sudo -E -H -u {0} {1}'.format(self.cron_user, self.cron_cmd), shell=True))
