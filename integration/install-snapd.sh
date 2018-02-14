ed1988422ee648c95cbc2b62aa32afc5a93362b8
Boris Rybalkin <ribalkin@gmail.com>
2018-02-14 07:56:21 +0000

    upgrade snapd to increase hooks timeout to 30 mins

--- integration/install-snapd.sh
+++ integration/install-snapd.sh
@@ -8,7 +8,7 @@ if [[ -z "$1" ]]; then
 fi
 
 ARCH=$(dpkg --print-architecture)
-VERSION=180205
+VERSION=180214
 
 SNAPD=snapd-${VERSION}-${ARCH}.tar.gz
 wget http://apps.syncloud.org/apps/${SNAPD} --progress=dot:giga
