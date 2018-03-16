86e5f67e5ace0585d4775a356492fae664f64bc2
Boris Rybalkin <ribalkin@gmail.com>
2018-03-16 19:09:21 +0000

    print headers

--- integration/verify.py
+++ integration/verify.py
@@ -107,6 +107,7 @@ def nextcloud_session_domain(user_domain, device_host):
     session = requests.session()
     response = session.get('http://{0}/index.php/login'.format(device_host), headers={"Host": user_domain}, allow_redirects=False)
     print(response.text)
+    print(response.headers)
     soup = BeautifulSoup(response.text, "html.parser")
     requesttoken = soup.find_all('input', {'name': 'requesttoken'})[0]['value']
     response = session.post('http://{0}/index.php/login'.format(device_host),
