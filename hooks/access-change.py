9a1493d4e2174fce88dcdef34ec62ee2542cff1d
Boris Rybalkin <ribalkin@gmail.com>
2018-11-09 17:51:45 +0000

    no platform python dependency

--- hooks/access-change.py
+++ hooks/access-change.py
@@ -1,2 +1,5 @@
-from nextcloud_hooks import NextcloudInstaller
-NextcloudInstaller().on_domain_change()
+import subprocess
+from os.path import join, dirname
+
+subprocess.check_output(join(dirname(__file__), 'access-change'), shell=True)
+
