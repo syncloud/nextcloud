48a5eb879b00c71db674450de0951953c2ba5350
Boris Rybalkin <ribalkin@gmail.com>
2021-06-17 23:55:12 +0100

    fix data dir

--- bin/php.sh
+++ bin/php.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+SNAP_DATA=/var/snap/nextcloud/current
 export LC_ALL=C
-exec ${DIR}/php/lib/ld.so --library-path ${DIR}/php/lib:${DIR}/php/lib/private ${DIR}/php/bin/php "$@"
+export NEXTCLOUD_CONFIG_DIR=${SNAP_DATA}/nextcloud/config
+exec ${DIR}/php/lib/ld.so --library-path ${DIR}/php/lib:${DIR}/php/lib/private ${DIR}/php/bin/php  -c ${SNAP_DATA}/config/php.in "$@"
