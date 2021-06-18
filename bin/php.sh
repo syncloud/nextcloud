7bf4be3123a1beba95495469d37c932d4fa34cef
Boris Rybalkin <ribalkin@gmail.com>
2021-06-18 13:51:58 +0100

    try LANG

--- bin/php.sh
+++ bin/php.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 SNAP_DATA=/var/snap/nextcloud/current
+export LANG=en_US.utf-8
+export LC_ALL=en_US.utf-8 
 export LC_ALL=en_GB.utf8
 export LC_TIME=en_GB.utf8
 export NEXTCLOUD_CONFIG_DIR=${SNAP_DATA}/nextcloud/config
