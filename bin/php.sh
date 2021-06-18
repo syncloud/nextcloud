2dae5a05661910e3a0f3d4e3bc69faa571a4081b
Boris Rybalkin <ribalkin@gmail.com>
2021-06-18 06:58:53 +0100

    try LC_TIME

--- bin/php.sh
+++ bin/php.sh
@@ -2,6 +2,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 SNAP_DATA=/var/snap/nextcloud/current
 export LC_ALL=C
-export LC_TIME°C
+export LC_TIME=C
 export NEXTCLOUD_CONFIG_DIR=${SNAP_DATA}/nextcloud/config
 exec ${DIR}/php/lib/ld.so --library-path ${DIR}/php/lib:${DIR}/php/lib/private ${DIR}/php/bin/php  -c ${SNAP_DATA}/config/php.in "$@"
+
