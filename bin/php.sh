7fee44845f1378ad9dfb7495bd5288ea5315abf1
Boris Rybalkin <ribalkin@gmail.com>
2021-06-18 07:58:38 +0100

    try en_GB.utf8

--- bin/php.sh
+++ bin/php.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 SNAP_DATA=/var/snap/nextcloud/current
-export LC_ALL=C
-export LC_TIME=C
+export LC_ALL=en_GB.utf8
+export LC_TIME=en_GB.utf8
 export NEXTCLOUD_CONFIG_DIR=${SNAP_DATA}/nextcloud/config
 exec ${DIR}/php/lib/ld.so --library-path ${DIR}/php/lib:${DIR}/php/lib/private ${DIR}/php/bin/php  -c ${SNAP_DATA}/config/php.in "$@"
 
