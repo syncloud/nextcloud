6036590a57ff08fb30b06ed8d09157592f7b4320
Boris Rybalkin <ribalkin@gmail.com>
2022-02-22 08:22:37 +0000

    php8 (https://github.com/syncloud/platform/issues/631) (#10)

---
+++ php/php.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*/samba)
+MAGICK_CODER_MODULE_PATH=$(echo ${DIR}/usr/lib/ImageMagickCoders) PHP_INI_SCAN_DIR=${DIR}/usr/local/etc/php/conf.d ${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/usr/local/bin/php "$@"
