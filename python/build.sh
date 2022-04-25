d04b090215b0168f71969bd3e6d7d9047081dad1
Boris Rybalkin <ribalkin@gmail.com>
2022-04-25 23:37:53 +0100

    23.0.4

--- python/build.sh
+++ python/build.sh
@@ -5,7 +5,7 @@ cd ${DIR}
 apt update
 apt install -y libltdl7 libnss3
 
-BUILD_DIR=${DIR}/../build/nextcloud/python
+BUILD_DIR=${DIR}/../build/snap/python
 docker ps -a -q --filter ancestor=python:syncloud --format="{{.ID}}" | xargs docker stop | xargs docker rm || true
 docker rmi python:syncloud || true
 docker build -t python:syncloud .
