c0f396c68a72660346818786a24c65cce5b4af85
Boris Rybalkin <ribalkin@gmail.com>
2022-04-30 22:31:53 +0100

    activate build in collabora

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -116,6 +116,7 @@ def test_users(selenium, app_domain):
 
 def test_(selenium, app_domain):
     selenium.driver.get('https://{0}/settings/admin/richdocuments'.format(app_domain))
+    selenium.find_by_xpath("//label[normalize-space(text())='Use the built-in CODE - Collabora Online Development Edition']").click()
     selenium.find_by_xpath("//span[normalize-space(text())='Collabora Online server is reachable.']")
     selenium.screenshot('office')
 
