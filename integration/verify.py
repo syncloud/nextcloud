f3a6143de49270035c25f49c59ba64407b009004
Boris Rybalkin <ribalkin@gmail.com>
2022-06-10 09:26:39 +0100

    retry install test

--- integration/verify.py
+++ integration/verify.py
@@ -270,15 +270,15 @@ def test_upgrade_from_store(device, app, app_archive_path, device_host, device_p
 
 
 def test_install_calendar(device):
-    device.run_ssh('snap run nextcloud.occ app:install calendar')
+    device.run_ssh('snap run nextcloud.occ app:install calendar', retries=10)
 
 
 def test_install_contacts(device):
-    device.run_ssh('snap run nextcloud.occ app:install contacts')
+    device.run_ssh('snap run nextcloud.occ app:install contacts', retries=10)
 
 
 def test_install_office(device, arch):
-    device.run_ssh('snap run nextcloud.occ app:install richdocuments')
+    device.run_ssh('snap run nextcloud.occ app:install richdocuments', retries=10)
 
 
 def test_upload_office_file(device, arch, device_user, device_password, app_domain):
