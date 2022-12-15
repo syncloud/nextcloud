856bb819efa835c055b4eabf3257d897f4d47ee7
Boris Rybalkin <ribalkin@gmail.com>
2022-12-15 17:40:15 +0000

    no progress on alpine

--- download.sh
+++ download.sh
@@ -15,6 +15,6 @@ wget -c --progress=dot:giga ${DOWNLOAD_URL}/nginx/nginx-${ARCH}.tar.gz
 tar xf nginx-${ARCH}.tar.gz
 mv nginx ${BUILD_DIR}
 
-wget --progress=dot:giga https://download.nextcloud.com/server/releases/nextcloud-${VERSION}.tar.bz2 -O nextcloud.tar.bz2
+wget https://download.nextcloud.com/server/releases/nextcloud-${VERSION}.tar.bz2 -O nextcloud.tar.bz2
 tar xf nextcloud.tar.bz2
 mv nextcloud ${BUILD_DIR}
