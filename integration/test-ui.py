910d83b02a68a0e64e001ba8b8987de43dc0e4a1
Boris Rybalkin <ribalkin@gmail.com>
2023-03-18 16:34:54 +0000

    try teardown

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -13,8 +13,9 @@ TMP_DIR = '/tmp/syncloud/ui'
 
 
 @pytest.fixture(scope="session")
-def module_setup(request, device, artifact_dir, ui_mode):
+def module_setup(request, device, artifact_dir, ui_mode, driver):
     def module_teardown():
+        driver.quit()
         device.activated()
         device.run_ssh('mkdir -p {0}'.format(TMP_DIR), throw=False)
         device.run_ssh('journalctl > {0}/journalctl.ui.{1}.log'.format(TMP_DIR, ui_mode), throw=False)
@@ -127,8 +128,3 @@ def test_office(selenium, app_domain):
     selenium.find_by_xpath("//input[@value='Save']").click()
     #selenium.find_by_xpath("//span[normalize-space(text())='Collabora Online server is reachable.']")
     selenium.screenshot('office-status')
-
-
-def test_teardown(driver):
-    driver.quit()
-
