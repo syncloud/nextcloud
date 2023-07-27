d227bb955015347805c47121e8c4c5a6e2f62612
Boris Rybalkin <ribalkin@gmail.com>
2023-07-27 19:25:46 +0100

    retry

--- php/build.sh
+++ php/build.sh
@@ -7,7 +7,10 @@ BUILD_DIR=${DIR}/../build/snap/php
 
 docker ps -a -q --filter ancestor=php:syncloud --format="{{.ID}}" | xargs docker stop | xargs docker rm || true
 docker rmi php:syncloud || true
-docker build -t php:syncloud .
+while ! docker build -t php:syncloud .; do
+  sleep 1
+  echo "retry"
+done
 docker run php:syncloud php -i
 docker create --name=php php:syncloud
 mkdir -p ${BUILD_DIR}
