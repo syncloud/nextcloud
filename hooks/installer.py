e8863390c9088341a6774247aaf9cc428cb0663c
Boris Rybalkin <ribalkin@gmail.com>
2023-08-01 16:41:24 +0100

    fix upgrade

--- hooks/installer.py
+++ hooks/installer.py
@@ -173,10 +173,11 @@ class Installer:
         self.log.info('upgrading nextcloud')
         self.occ.run('maintenance:mode --on')
         self.occ.run('upgrade')
-        self.occ.run('app:update --all')
+        # self.occ.run('app:update --all')
         self.occ.run('maintenance:mode --off')
         self.occ.run('db:add-missing-indices')
         self.occ.run('db:add-missing-columns')
+        self.occ.run('db:add-missing-primary-keys')
         # else:
         #     self.log.info('not upgrading nextcloud')
 
