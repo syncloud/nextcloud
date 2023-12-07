243cdfba2ad37d240de3c5540449c4166006e167
Boris Rybalkin <ribalkin@gmail.com>
2023-12-07 18:05:32 +0000

    clean logs

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -2,24 +2,6 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-if [[ -z "$1" ]]; then
-    echo "usage $0 [start|stop]"
-    exit 1
-fi
-
-case $1 in
-start)
-    /bin/rm -f ${SNAP_COMMON}/web.socket
-    exec ${DIR}/nginx/sbin/nginx -c ${SNAP_DATA}/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
-    ;;
-reload)
-    ${DIR}/nginx/sbin/nginx -c ${SNAP_DATA}/config/nginx.conf -s reload -p ${DIR}/nginx
-    ;;
-stop)
-    ${DIR}/nginx/sbin/nginx -c ${SNAP_DATA}/config/nginx.conf -s stop -p ${DIR}/nginx
-    ;;
-*)
-    echo "not valid command"
-    exit 1
-    ;;
-esac
+/bin/rm -f ${SNAP_COMMON}/web.socket
+/bin/rm -f ${SNAP_COMMON}/log/nginx*.log
+exec ${DIR}/nginx/sbin/nginx -c ${SNAP_DATA}/config/nginx.conf -p ${DIR}/nginx -e stderr
