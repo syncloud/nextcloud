b975b03b5210e3adcf0d8daa7ff667ba6a8fc142
Boris Rybalkin <ribalkin@gmail.com>
2025-10-20 08:32:27 +0100

    deps

--- test/deps.sh
+++ test/deps.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
+DIR=$( cd "$( dirname "$0" )" && pwd )
 
-apt-get update
-apt-get install -y sshpass openssh-client netcat rustc file libxml2-dev libxslt-dev build-essential libz-dev curl
+while ! apt-get update; do
+  sleep 1
+  echo "retry"
+done
+apt-get install -y sshpass openssh-client wget
 pip install -r requirements.txt
