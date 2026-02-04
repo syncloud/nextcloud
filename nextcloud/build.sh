4af454b51bb80b9cf5980d7966d4758390899067
Boris Rybalkin <ribalkin@gmail.com>
2026-02-04 18:47:11 +0000

    fix user ini

--- nextcloud/build.sh
+++ nextcloud/build.sh
@@ -4,4 +4,4 @@ DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
 BUILD_DIR=${DIR}/../build/snap/nextcloud
 mkdir -p $BUILD_DIR
-cp -r /usr/src/nextcloud/* ${BUILD_DIR}/
+cp -r /usr/src/nextcloud/. ${BUILD_DIR}/
